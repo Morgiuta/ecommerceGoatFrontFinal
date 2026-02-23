488765ee1f7657de9b43f36b3497dd939b8e42f0
tomas garcia <tomasgarciacaffieri@gmail.com>
2026-02-23 01:58:46 -0300

    agrego funcionalidad descargar pdf

--- pages/shop/Pdf.tsx
+++ pages/shop/Pdf.tsx
@@ -1,7 +1,11 @@
 import jsPDF from "jspdf";
 
-export const generarPDF = (monto,fecha,nroPedido) => {
+export const generarPDF = (monto, fecha, nroPedido) => {
   const doc = new jsPDF();
-  doc.text("Hola Tomas 🚀", 10, 10);
+
+  doc.text(`Monto: $${monto}`, 10, 10);
+  doc.text(`Fecha: ${fecha}`, 10, 20);
+  doc.text(`Pedido N°: ${nroPedido}`, 10, 30);
+
   doc.save("archivo.pdf");
 };
